bf6e38ae568602fa7053f92d2cafddd9759b8da3
Intyre <Intyre@gmail.com>
2017-06-21 23:48:42 +0200

    Removed DB request and added pretty errors

--- app/handler/view.go
+++ app/handler/view.go
@@ -14,45 +14,52 @@ import (
 )
 
 func ViewIDGet(app *app.App) http.HandlerFunc {
-	const name = "view"
+	name := "view"
 	tmpl, err := template.LoadTemplate(name, app.Config.Template)
 	if err != nil {
 		log.Fatalf("failed to load template %s: %v\n", name, err)
 	}
 
-	querySelect := `SELECT * FROM crash_reports WHERE id = ?`
-	// queryTotal := `SELECT COUNT(*) FROM crash_reports`
+	name = "error"
+	errorTmpl, err := template.LoadTemplate(name, app.Config.Template)
+	if err != nil {
+		log.Fatalf("failed to load template %s: %v\n", name, err)
+	}
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		reportID, err := strconv.Atoi(chi.URLParam(r, "reportID"))
 		if err != nil {
-			log.Printf("%v\n", err)
+			errorTmpl.ExecuteTemplate(w, "base.html", map[string]interface{}{
+				"Message": "Please specify a report",
+				"URL":     "/",
+			})
 			return
 		}
 
-		data := crashreport.Report{}
-		err = app.Database.Get(&data, querySelect, reportID)
-		if err != nil {
-			log.Println(err)
-			return
-		}
 		report, jsonData, err := crashreport.ReadFile(int64(reportID))
 		if err != nil {
-			log.Println(err)
+			errorTmpl.ExecuteTemplate(w, "base.html", map[string]interface{}{
+				"Message": "Report not found",
+				"URL":     "/",
+			})
 			return
 		}
 
-		v := make(map[string]interface{}, 0)
-		v["Data"] = data
+		v := make(map[string]interface{})
 		v["Report"] = report
 		v["Name"] = clean(jsonData["name"].(string))
 		v["PocketMineVersion"] = report.Version.Get(true)
 		v["AttachedIssue"] = "None"
-		tmpl.ExecuteTemplate(w, "base.html", v)
+		v["ReportID"] = reportID
+
+		if err = tmpl.ExecuteTemplate(w, "base.html", v); err != nil {
+			log.Println(err)
+		}
 	}
 }
 
+var cleanRE = regexp.MustCompile(`[^A-Za-z0-9_\-\.\,\;\:/\#\(\)\\ ]`)
+
 func clean(v string) string {
-	var re = regexp.MustCompile("[^A-Za-z0-9_\\-\\.\\,\\;\\:/\\#\\(\\)\\\\ ]")
-	return re.ReplaceAllString(v, "")
+	return cleanRE.ReplaceAllString(v, "")
 }
