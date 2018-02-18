a473d26046efd242ee72e93e9055a77b9a3d0eda
Dylan K. Taylor <odigiman@gmail.com>
2018-02-18 14:23:50 +0000

    Fixed some code style issues in version string test

--- app/crashreport/versionstring_test.go
+++ app/crashreport/versionstring_test.go
@@ -6,9 +6,9 @@ func TestVersionString(t *testing.T) {
 	testVersions := [...]string{"1.6.2dev-178", "1.6", "1.6dev", "1.6.2dev", "1.6.2-1800"}
 
 	for _, versionText := range testVersions {
-		version := NewVersionString(versionText, 0);
-		if(version.Get(true) != versionText){
-			t.Fatalf("Bad version string, expected %s, got %s", versionText, version.Get(true));
+		version := NewVersionString(versionText, 0)
+		if version.Get(true) != versionText {
+			t.Fatalf("Bad version string, expected %s, got %s", versionText, version.Get(true))
 		}
 	}
 }
